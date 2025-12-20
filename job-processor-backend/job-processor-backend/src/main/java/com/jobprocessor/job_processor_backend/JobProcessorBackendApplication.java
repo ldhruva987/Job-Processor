1d35220286273d80e215cbd0830ccf84fe685473
ldhruva987 <varahalababuakadhruva@gmail.com>
2025-12-20 15:57:03 +0530

    Adding Backend for Distributed Task and Job processor

---
+++ job-processor-backend/job-processor-backend/src/main/java/com/jobprocessor/job_processor_backend/JobProcessorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jobprocessor.job_processor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobProcessorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobProcessorBackendApplication.class, args);
+	}
+
+}
